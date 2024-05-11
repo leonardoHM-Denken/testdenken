2e7b5c66c083ccb29c19973be6951533af594675
leonardoHM-Denken <leonardo.hernandez@denkentec.com>
2024-05-11 23:50:15 +0000

    [ADD] New Course Model

--- motorcycle_registry/models/course.py
+++ motorcycle_registry/models/course.py
@@ -5,3 +5,24 @@ from odoo import models
 class Course(models.Model):
     _name = 'motorcycle_registry.course'
     _description = 'Registro de Motocicletas'
+
+    name = fields.char(string="Title", required=True)
+    active = fields.Boolean(string="Active", default=True)
+
+    description = fields.Text()
+    level = fields.selection(string="Level",
+                             selection=[
+                                 ('begginer', 'Begginer'),
+                                 ('intermediate', 'Intermediate'),
+                                 ('advanced', 'Advanced'),
+                             ],
+                             copy=False)
+
+    certificate_title = fields.binary(string='Binary File')
+    current_mileage = fields.Float(string='Current Mileage')
+    first_name = fields.char(string="First Name", required=True)
+    last_name = fields.char(string="Last Name", required=True)
+    license_platte = fields.char(string="License Plate")
+    registry_date = fields.Date(string='Registry Date')
+    registry_number = fields.Char(string='Registry Number', required=True)
+    vin = fields.Char(string='VIN', required=True)
