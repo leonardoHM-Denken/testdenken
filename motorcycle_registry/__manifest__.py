642b741fa065b586d1e86d769ef7faffc9ebe98b
leonardoHM-Denken <leonardo.hernandez@denkentec.com>
2024-05-11 21:29:53 +0000

    [ADD] New Module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Motorcyle Registry",
+    'summary': "Manage Registration Motorcycle",
+    'description': """
+        Motorcycle Registry
+        ===================
+        Este módulo es para registrar motos
+    """,
+    'author': "LHM",
+    'website': "www.odoo.com",
+    'category': 'Kawiil/registry',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+}
