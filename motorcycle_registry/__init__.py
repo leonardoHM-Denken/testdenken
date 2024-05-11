aa620eb881e933b12b24a51e87effaf9b75adbfb
leonardoHM-Denken <leonardo.hernandez@denkentec.com>
2024-05-11 23:11:46 +0000

    [ADD] New Course Model

--- motorcycle_registry/__init__.py
+++ motorcycle_registry/__init__.py
@@ -1,1 +1,1 @@
-
+from . importe models
---
+++ motorcycle_registry/models/course.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+class Course(models.Model):
+    _name = 'motorcycle_registry.course'
+    _description = 'Registro de Motocicletas'
