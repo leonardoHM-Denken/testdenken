17250495fa3de99a064a204f056f29637fc289ab
leonardoHM-Denken <leonardo.hernandez@denkentec.com>
2024-05-12 00:21:58 +0000

    [ADD] New Course Module

--- motorcycle_registry/models/course.py
+++ motorcycle_registry/models/course.py
@@ -6,11 +6,11 @@ class Course(models.Model):
     _name = 'motorcycle_registry.course'
     _description = 'Registro de Motocicletas'
 
-    name = fields.char(string="Title", required=True)
+    name = fields.Char(string="Title", required=True)
     active = fields.Boolean(string="Active", default=True)
 
     description = fields.Text()
-    level = fields.selection(string="Level",
+    level = fields.Selection(string="Level",
                              selection=[
                                  ('begginer', 'Begginer'),
                                  ('intermediate', 'Intermediate'),
@@ -18,11 +18,11 @@ class Course(models.Model):
                              ],
                              copy=False)
 
-    certificate_title = fields.binary(string='Binary File')
-    current_mileage = fields.float(string='Current Mileage')
-    first_name = fields.char(string="First Name", required=True)
-    last_name = fields.char(string="Last Name", required=True)
-    license_platte = fields.char(string="License Plate")
-    registry_date = fields.date(string='Registry Date')
-    registry_number = fields.char(string='Registry Number', required=True)
+    certificate_title = fields.Binary(string='Binary File')
+    current_mileage = fields.Float(string='Current Mileage')
+    first_name = fields.Char(string="First Name", required=True)
+    last_name = fields.Char(string="Last Name", required=True)
+    license_platte = fields.Char(string="License Plate")
+    registry_date = fields.Date(string='Registry Date')
+    registry_number = fields.Char(string='Registry Number', required=True)
     vin = fields.Char(string='VIN', required=True)
